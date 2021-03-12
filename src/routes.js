93ff0f6d14c379f61323cb56be33b0e535c2a54a
Aleydon <aleydonroberto@gmail.com>
2021-03-12 15:59:34 -0300

    Create post route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+import UserController from './controllers/User.controller';
+
 const route = Router();
 
-route.get('/', (req, res) => console.log('Get Route'));
+route.get('/', UserController.index);
+route.post('/', UserController.store);
 
 export default route;
