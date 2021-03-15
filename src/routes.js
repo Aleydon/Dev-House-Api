afa007b29be463ad05ef95f85bc2e340ab1b01e8
Aleydon <aleydonroberto@gmail.com>
2021-03-15 03:43:35 -0300

    Added houses post route with upload

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import configUpload from './config/multer';
 import UserController from './controllers/UserController';
+import HouseController from './controllers/HouseController';
 
 const route = Router();
+const upload = multer(configUpload);
 
+// User / Session routes
 route.get('/', UserController.index);
 route.post('/', UserController.store);
 route.delete('/:id', UserController.destroy);
 route.put('/:id', UserController.update);
+
+// House routes
+route.post('/houses', upload.single('thumbnail'), HouseController.store);
 
 export default route;
